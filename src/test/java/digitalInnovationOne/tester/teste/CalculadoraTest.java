10ea5ec81f8dafe87f0e9dad3a960f390783da57
Andrea Cruz <andrea@treinamento.com>
2021-07-22 12:01:55 -0300

    Commit classes tester

---
+++ src/test/java/digitalInnovationOne/tester/teste/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package digitalInnovationOne.tester.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void testSomar() {
+		fail("Not yet implemented");
+	}
+
+}
