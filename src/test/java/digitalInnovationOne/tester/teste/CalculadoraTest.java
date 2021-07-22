550c927264c7a01e2b6b00aea436055ca7b1291a
Andrea Cruz <andrea@treinamento.com>
2021-07-22 20:33:38 -0300

    Mocks, junit e TDD

--- src/test/java/digitalInnovationOne/tester/teste/CalculadoraTest.java
+++ src/test/java/digitalInnovationOne/tester/teste/CalculadoraTest.java
@@ -1,6 +1,8 @@
 package digitalInnovationOne.tester.teste;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -11,6 +13,14 @@ public class CalculadoraTest {
 		Calculadora calc = new Calculadora();
 		int soma=  calc.somar("5+3+2");
 		assertEquals(10, soma);
+	}
+	@Test
+	public void testeSomarComMock() {
+		Calculadora calc=  mock(Calculadora.class);
+		when(calc.somar("1+2")).thenReturn(10);
+		int resultado = calc.somar("1+2");
+		assertEquals(10, resultado);
+		
 	}
 
 }
